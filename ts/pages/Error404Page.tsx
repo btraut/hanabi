e99c825b9b2d89efdac1fce20455dfa174c36157
Brent Traut <brent@traut.com>
2018-01-24 00:28:42 -0800

    Hooked up the 404 page.

--- ts/pages/Error404Page.tsx
+++ ts/pages/Error404Page.tsx
@@ -1,8 +1,23 @@
 import * as React from 'react';
+import { RouteProps } from 'react-router';
 
-export default () => (
-	<div>
-		<h1 className="Error404Page-SectionHeader">404 &ndash; Page Not Found</h1>
-		<p className="Error404Page-Message">Looks like our pipes are broken. The page you requested doesn’t exist. Sorry about that!</p>
-	</div>
-);
+export default class Error404Page extends React.PureComponent<RouteProps> {
+	public static async preload() {
+		console.log('404 preloaded');
+	}
+	
+	public static title = 'Lost in Translation | 404';
+	
+	public componentDidMount() {
+		document.title = Error404Page.title;
+	}
+	
+	public render() {
+		return (
+			<div>
+				<h1 className="Error404Page-SectionHeader">404 &ndash; Page Not Found</h1>
+				<p className="Error404Page-Message">Looks like our pipes are broken. The page you requested doesn’t exist. Sorry about that!</p>
+			</div>
+		);
+	}
+};
--- ts/routes.tsx
+++ ts/routes.tsx
@@ -1,6 +1,7 @@
 import { RouteProps } from 'react-router';
 
 import { PageClass } from './pages/Page';
+import Error404Page from './pages/Error404Page';
 import HomePage from './pages/HomePage';
 
 type AsyncRouteProps = RouteProps & {
@@ -9,7 +10,11 @@ type AsyncRouteProps = RouteProps & {
 
 const routes: AsyncRouteProps[] = [{
 	path: '/',
-	component: HomePage
+	component: HomePage,
+	exact: true
+}, {
+	path: '*',
+	component: Error404Page
 }];
 
 export default routes;
