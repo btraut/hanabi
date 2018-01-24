307b82a3712b67b2ebdd3482c8663c543f5f5689
Brent Traut <brent@traut.com>
2018-01-23 19:40:07 -0800

    Updated ScrollRestoration to pass props through to children.

--- ts/components/ScrollRestoration.tsx
+++ ts/components/ScrollRestoration.tsx
@@ -1,17 +1,24 @@
-import { PureComponent } from 'react';
+import { cloneElement, Children, PureComponent } from 'react';
 import * as ReactRouter from 'react-router';
 import { withRouter } from 'react-router-dom';
 
 class ScrollToTop<P extends ReactRouter.RouteComponentProps<any>> extends PureComponent<P> {
-    public componentDidUpdate(prevProps: P) {
-        if (this.props.location !== prevProps.location) {
-            window.scrollTo(0, 0);
-        }
-    }
+	public componentDidUpdate(prevProps: P) {
+		if (this.props.location !== prevProps.location) {
+			window.scrollTo(0, 0);
+		}
+	}
 
-    public render() {
-        return this.props.children;
-    }
+	public render() {
+		// Pass props through this component to children.
+		return Children.map(this.props.children, child => {
+			if (typeof child === 'string' || typeof child === 'number') {
+				return child;
+			}
+			
+			return cloneElement(child, this.props);
+		});
+	}
 }
 
 export default withRouter(ScrollToTop);
