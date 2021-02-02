c60f1ec1cf45b3038d16efae27ec1b302aee6e37
Brent Traut <brent@traut.com>
2021-02-02 00:49:09 -0800

    Added hamburger button

--- app/src/games/hanabi/client/HanabiHeader.tsx
+++ app/src/games/hanabi/client/HanabiHeader.tsx
@@ -1,9 +1,13 @@
+import Hamburger from 'app/src/games/hanabi/client/icons/Hamburger';
+
 export default function HanabiHeader(): JSX.Element {
 	return (
 		<div className="bg-black">
 			<div className="mx-auto max-w-screen-xl px-4 flex justify-between items-center">
 				<h1 className="text-white italic font-bold text-3xl px-3 py-2">Hanabi</h1>
-				<button className="text-white p-3">Menu</button>
+				<button className="text-white p-3">
+					<Hamburger size={20} color="white" />
+				</button>
 			</div>
 		</div>
 	);
