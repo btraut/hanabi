f96aa787cf98120cd868e4300bc6511e930f3e85
Brent Traut <brent@traut.com>
2021-05-01 23:10:52 -0700

    Fixed sounds on Safari

--- app/src/games/hanabi/HanabiPage.tsx
+++ app/src/games/hanabi/HanabiPage.tsx
@@ -5,6 +5,7 @@ import HanabiOptionsController from 'app/src/games/hanabi/client/HanabiOptionsCo
 import HanabiRouter from 'app/src/games/hanabi/client/HanabiRouter';
 import HanabiStyles from 'app/src/games/hanabi/client/HanabiStyles';
 import Page from 'app/src/pages/Page';
+import useAudioUnlock from 'app/src/utils/client/useAudioUnlock';
 import useSetTitle from 'app/src/utils/client/useSetTitle';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
@@ -13,6 +14,8 @@ const title = 'Hanabi | Play';
 
 const HanabiPage: Page = () => {
 	useSetTitle(title);
+
+	useAudioUnlock();
 
 	return (
 		<SocketManagerController>
